7d0f6c0df20b13d4020be3eb2a619cdb4fbe9f59
Lukas Lieb <lukas.lieb@axonivy.com>
2025-05-09 14:15:21 +0200

    Cleanup vite configs

--- packages/variable-editor/vite.config.ts
+++ packages/variable-editor/vite.config.ts
@@ -1,15 +1,13 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [react(), dts({ tsconfigPath: './tsconfig.production.json' })],
-  esbuild: {
-    sourcemap: 'inline'
-  },
   build: {
     outDir: 'lib',
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       fileName: 'editor',
@@ -29,5 +27,17 @@ export default defineConfig({
         'react-dom'
       ]
     }
+  },
+  test: {
+    dir: 'src',
+    include: ['**/*.test.ts?(x)'],
+    alias: {
+      '@axonivy/variable-editor-protocol': resolve(__dirname, '../../packages/protocol/src')
+    },
+    globals: true,
+    environment: 'jsdom',
+    css: false,
+    reporters: process.env.CI ? ['default', 'junit'] : ['default'],
+    outputFile: 'report.xml'
   }
 });
