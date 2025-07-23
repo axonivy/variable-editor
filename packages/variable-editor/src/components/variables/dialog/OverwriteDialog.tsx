688af6d3d0200a5f8efe58ec23221c1e8dd2d9f3
Lukas Lieb <lukas.lieb@axonivy.com>
2025-07-23 12:31:46 +0200

    XIVY-16606 Apply basic dialog changes

--- packages/variable-editor/src/components/variables/dialog/OverwriteDialog.tsx
+++ packages/variable-editor/src/components/variables/dialog/OverwriteDialog.tsx
@@ -1,19 +1,21 @@
 import {
-  BasicDialog,
+  BasicDialogHeader,
   Button,
+  Dialog,
+  DialogContent,
   DialogTrigger,
+  Flex,
   selectRow,
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-  useHotkeyLocalScopes,
+  useDialogHotkeys,
   useHotkeys
 } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
 import type { KnownVariables } from '@axonivy/variable-editor-protocol';
 import { type Table } from '@tanstack/react-table';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAppContext } from '../../../context/AppContext';
 import { toRowId } from '../../../utils/tree/tree';
@@ -23,26 +25,12 @@ import { addKnownVariable } from './known-variables';
 import './OverwriteDialog.css';
 import { VariableBrowser } from './VariableBrowser';
 
-type OverwriteProps = {
-  table: Table<Variable>;
-};
-
-export const OverwriteDialog = ({ table }: OverwriteProps) => {
+export const OverwriteDialog = ({ table }: { table: Table<Variable> }) => {
   const { t } = useTranslation();
   const { setVariables, setSelectedVariable } = useAppContext();
-  const [dialogState, setDialogState] = useState(false);
-  const { activateLocalScopes, restoreLocalScopes } = useHotkeyLocalScopes(['overwriteDialog']);
+  const { open, onOpenChange } = useDialogHotkeys(['overwriteDialog']);
   const { importVar: shortcut } = useKnownHotkeys();
-  const onOpenChange = (open: boolean) => {
-    setDialogState(open);
-    if (open) {
-      activateLocalScopes();
-    } else {
-      restoreLocalScopes();
-    }
-  };
-
-  useHotkeys(shortcut.hotkey, () => onOpenChange(true), { scopes: ['global'], keyup: true, enabled: !dialogState });
+  useHotkeys(shortcut.hotkey, () => onOpenChange(true), { scopes: ['global'], keyup: true, enabled: !open });
 
   const insertVariable = (node?: KnownVariables): void => {
     if (!node) {
@@ -57,33 +45,28 @@ export const OverwriteDialog = ({ table }: OverwriteProps) => {
   };
 
   return (
-    <BasicDialog
-      open={dialogState}
-      onOpenChange={onOpenChange}
-      contentProps={{
-        title: t('dialog.overwrite.title'),
-        description: t('dialog.overwrite.desc'),
-        className: 'variables-editor-overwrite-dialog-content'
-      }}
-      dialogTrigger={
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <DialogTrigger asChild>
-                <Button icon={IvyIcons.FileImport} aria-label={shortcut.label} />
-              </DialogTrigger>
-            </TooltipTrigger>
-            <TooltipContent>{shortcut.label}</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      }
-    >
-      <VariableBrowser
-        applyFn={node => {
-          insertVariable(node);
-          setDialogState(false);
-        }}
-      />
-    </BasicDialog>
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <DialogTrigger asChild>
+              <Button icon={IvyIcons.FileImport} aria-label={shortcut.label} />
+            </DialogTrigger>
+          </TooltipTrigger>
+          <TooltipContent>{shortcut.label}</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <DialogContent className='variables-editor-overwrite-dialog-content'>
+        <Flex direction='column' gap={4}>
+          <BasicDialogHeader title={t('dialog.overwrite.title')} description={t('dialog.overwrite.desc')} />
+          <VariableBrowser
+            applyFn={node => {
+              insertVariable(node);
+              onOpenChange(false);
+            }}
+          />
+        </Flex>
+      </DialogContent>
+    </Dialog>
   );
 };
